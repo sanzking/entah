8e29377a2f8f1058ed4cd36a4f26d96074d066ea
Muhammad Shohibul Faqih <70243455+sanzking@users.noreply.github.com>
2023-03-31 20:25:01 +0700

    Delete static directory

--- static/script.js
+++
@@ -1,9 +0,0 @@
-// Script untuk menampilkan sub-menu pada tampilan mobile
-const menuIcon = document.querySelector('.menu-icon');
-const menuList = document.querySelector('nav ul');
-menuIcon.addEventListener('click', () => {
-    menuList.classList.toggle('show-menu');
-});
-
-// Script untuk menyalin value pada input form
-new ClipboardJS('.copy-button');
